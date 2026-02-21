4c57c24852beff6ec98af343d98337496511d934
Yiftah Waisman <yiftah.waisman@forsightrobotics.com>
2026-02-21 17:41:57 +0200

    use pixi as env provider

--- pybind11/pose_module.cpp
+++ pybind11/pose_module.cpp
@@ -1,57 +1,27 @@
 // pose_module.cpp
+#include <format>
 #include <concepts>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-class Pose {
-public:
-    Pose() : x(0), y(0), z(0) {}
-
-    void set_values(float x_, float y_, float z_) {
-        x = x_; y = y_; z = z_;
-    }
-
-    std::string to_string() const {
-        return "Pose(" + std::to_string(x) + ", " + 
-                         std::to_string(y) + ", " +
-                         std::to_string(z) + ")";
-    }
-
-    float& get_x() { return x; }
-    float& get_y() { return y; }
-    float& get_z() { return z; }
-
-    void set_x(float _x) { x = _x; }
-    void set_y(float _y) { y = _y; }
-    void set_z(float _z) { z = _z; }
-private:
+struct Pose {
     float x, y, z;
+    std::string to_string() const {
+        return std::format("({:.3f}, {:.3f}, {:.3f})", x, y, z);
+    }
 };
 static_assert(std::is_trivially_copyable_v<Pose>, "Pose must be trivially copyable");
-
-class Line {
-public:
-    Pose start, end;
-    Line(const Pose& start_, const Pose& end_) : start(start_), end(end_) {}
-    std::string to_string() const {
-        return "Line(" + start.to_string() + ", " + end.to_string() + ")";
-    }
-};
-
-static_assert(std::is_trivially_copyable_v<Line>, "Line must be trivially copyable");
-
-Pose& get_start(Line& self) { return self.start; }
-Pose& get_end(Line& self) { return self.end; }
+static_assert(std::is_standard_layout_v<Pose>, "Pose must have standard layout");
 
 // templated function that automatically adds buffer protocol support (as raw byte buffers)
 // to any PoD like struct
-template <typename T, typename... CtorArgs>
+template <typename T>
     requires std::is_trivially_copyable_v<T> && std::is_standard_layout_v<T>
 py::class_<T> bind_buffer(py::module_ &m, const char *name)
 {
     return py::class_<T>(m, name, py::buffer_protocol())
-        .def(py::init<CtorArgs...>())
+        .def(py::init<>())
         .def_buffer([](T &self) -> py::buffer_info
                     { return py::buffer_info(
                         // expose as a raw byte buffer
@@ -73,14 +43,8 @@ PYBIND11_MODULE(pose_module, m) {
     // which is useful for IPC on the same machine.
 
     bind_buffer<Pose>(m, "Pose")
-        .def("set_values", &Pose::set_values, "Set x, y, z coordinates")
-        .def_property("x", &Pose::get_x, &Pose::set_x, "X coordinate of the pose")
-        .def_property("y", &Pose::get_y, &Pose::set_y, "Y coordinate of the pose")
-        .def_property("z", &Pose::get_z, &Pose::set_z, "Z coordinate of the pose")
+        .def_readwrite("x", &Pose::x)
+        .def_readwrite("y", &Pose::y)
+        .def_readwrite("z", &Pose::z) 
         .def("__repr__", &Pose::to_string);
-
-    bind_buffer<Line, const Pose&, const Pose&>(m, "Line")
-        .def("get_start", &get_start, py::return_value_policy::reference_internal, "Get start Pose of the line")
-        .def("get_end", &get_end, py::return_value_policy::reference_internal, "Get end Pose of the line")
-        .def("__repr__", &Line::to_string);
 }
